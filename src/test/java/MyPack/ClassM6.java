bdfc3653bccf4c98b9106ca829a323bd366325df
MeghanathK <meghanath11me054@gmail.com>
2022-02-09 19:18:19 +0530

    6

---
+++ src/test/java/MyPack/ClassM6.java
@@ -0,0 +1,20 @@
+package MyPack;
+
+import org.testng.annotations.Test;
+
+public class ClassM6 {
+	
+	@Test
+	public void Method1() {
+
+	//Git Control
+
+	}
+
+@Test	
+public void methodM3()
+{
+	
+	System.out.println("Megu3");
+}
+}
