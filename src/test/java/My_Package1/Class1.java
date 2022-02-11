c8fe2b5959bc63cac64ac85f0fee19d8b4ae5016
MeghanathK <meghanath11me054@gmail.com>
2022-02-11 16:52:02 +0530

    new

---
+++ src/test/java/My_Package1/Class1.java
@@ -0,0 +1,32 @@
+package My_Package1;
+
+public class Class1 {
+	
+	
+	public void Method1()
+	{
+		System.out.println("M1");
+		
+	}
+	
+	
+	public void Method2()
+	{
+		System.out.println("M1");
+		
+	}
+
+	public void Method3()
+	{
+		System.out.println("M1");
+		
+	}
+
+	public void Method4()
+	{
+		System.out.println("M1");
+		
+	}
+
+
+}
