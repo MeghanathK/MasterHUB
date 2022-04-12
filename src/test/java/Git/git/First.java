41019b4a9d8880593da4679f24285aea4eb87710
MeghanathK <meghanath11me054@gmail.com>
2022-04-12 20:31:13 +0530

    meghanath

---
+++ src/test/java/Git/git/First.java
@@ -0,0 +1,15 @@
+package Git.git;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int ten =10;
+		int twenty =20;
+				
+	}
+
+}
